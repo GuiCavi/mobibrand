5b8c6df2852d2ff662e7a49a3bb3b57dc4ddaf8a
Guilherme Vincenzi Cavichioli <guilhermecavichioli@hotmail.com>
2017-12-10 02:29:51 -0200

    Merge

--- src/index.js
+++ src/index.js
@@ -5,10 +5,6 @@ import App from './App';
 import Editor from './components/Editor';
 import registerServiceWorker from './registerServiceWorker';
 
-<<<<<<< HEAD
 ReactDOM.render(<App />, document.getElementById('root'));
 // ReactDOM.render(<Editor useModel={true} type={"board"} boardType={"sell"} phone={"(19) 4132-1232"} name={"Imobiliaria"} email={"imob@teste.com"} creci={"413.21"} />, document.getElementById('root'));
-=======
-ReactDOM.render(<Editor useModel={true} type={"banner_3"} boardType={"sell"} phone={"(19) 4132-1232"} name={"Imobiliaria"} email={"imob@teste.com"} site={"www.realestate.com.br"} creci={"413.21"} />, document.getElementById('root'));
->>>>>>> 26be9ef2c26adf68ef4eebaea9a665059a15e9ed
 registerServiceWorker();
